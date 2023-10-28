d787e9aabc337b6ff2bd1f769ff36cf91d177cac
Kiryuha161 <108215760+Kiryuha161@users.noreply.github.com>
2023-10-28 22:50:24 +0300

    Сделал редактирование категории

--- BooksWeb/Controllers/CategoryController.cs
+++ BooksWeb/Controllers/CategoryController.cs
@@ -11,12 +11,72 @@ namespace BooksWeb.Controllers
         {
             _database = database;
         }
-        
+
+        [HttpGet]
         public IActionResult Index()
         {
             IEnumerable<Category> categoryList = _database.Categories;
-            
+
             return View(categoryList);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDatabase = _database.Categories.Find(id);
+
+            if (categoryFromDatabase == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDatabase);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Имя не может совпадать с порядковым номером");
+            }
+            if (ModelState.IsValid)
+            {
+                _database.Categories.Add(obj);
+                _database.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Имя не может совпадать с порядковым номером");
+            }
+            if (ModelState.IsValid)
+            {
+                _database.Categories.Update(obj);
+                _database.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View(obj);
         }
     }
 }
+
--- BooksWeb/Models/Domain/Category.cs
+++ BooksWeb/Models/Domain/Category.cs
@@ -7,8 +7,12 @@ namespace BooksWeb.Models.Domain
         [Key]
         public int Id { get; set; }
         
-        [Required]
+        [Required(ErrorMessage = "Поле \"Название\" обязательно к заполнению")]
+        [Display(Name="Название")]
         public string Name { get; set; }
+        [Display(Name="Порядковый номер")]
+        [Range(1, 100, ErrorMessage = "Порядковый номер может находится только между 1 и 100")]
+        [Required(ErrorMessage = "Поле \"Порядковый номер\" обязательно к заполнению")]
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateCategory { get; set; } = DateTime.Now;
 
