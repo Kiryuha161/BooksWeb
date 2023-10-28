84ab32e644d1ce7bc5f61610d1e94a59f6741942
Kiryuha161 <108215760+Kiryuha161@users.noreply.github.com>
2023-10-28 17:55:49 +0300

    Добавьте файлы проекта.

---
+++ BooksWeb/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using BooksWeb.Data;
+using BooksWeb.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _database;
+        public CategoryController(ApplicationDbContext database)
+        {
+            _database = database;
+        }
+        
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categoryList = _database.Categories;
+            
+            return View(categoryList);
+        }
+    }
+}
---
+++ BooksWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BooksWeb.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
